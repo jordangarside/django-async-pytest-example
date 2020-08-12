fdb8d78796fa160191b1d3f5e86ce025d187dfeb
Jordan Garside <jordan.garside@robinhood.com>
2020-08-12 11:58:59 -0700

    [tests] add sync test and add additional async case

--- tests/test_db_sync.py
+++ tests/test_db_sync.py
@@ -3,25 +3,21 @@ from asgiref.sync import sync_to_async
 from app.django_models.person.models import Person
 
 
-@sync_to_async
 def create_person() -> Person:
     return Person.objects.create(name="hello world")
 
 
-@sync_to_async
 def count_persons() -> int:
     return Person.objects.count()
 
 
-@pytest.mark.asyncio
 @pytest.mark.django_db()
-async def test_db() -> None:
-    await create_person()
-    assert await count_persons() == 1
+def test_db() -> None:
+    create_person()
+    assert count_persons() == 1
 
 
-@pytest.mark.asyncio
 @pytest.mark.django_db()
-async def test_db_again() -> None:
-    await create_person()
-    assert await count_persons() == 1
+def test_db_again() -> None:
+    create_person()
+    assert count_persons() == 1
